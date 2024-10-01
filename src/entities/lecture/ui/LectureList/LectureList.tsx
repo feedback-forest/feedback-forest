f03813e12dff7fbf19a4cb9c328ef3d8bd6da475
Lawrene Jang <jkb2221@gmail.com>
2024-10-01 13:32:44 +0900

    🐛 fix: delete unnecessary AND operator

--- src/entities/lecture/ui/LectureList/LectureList.tsx
+++ src/entities/lecture/ui/LectureList/LectureList.tsx
@@ -9,29 +9,27 @@ interface LectureListProps {
 const LectureList = ({ lectureListData, type }: LectureListProps) => {
   return type === "col" ? (
     <div className="w-full grid grid-cols-3 gap-6 mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3">
-      {lectureListData &&
-        lectureListData.map((lectureData) => {
-          return (
-            <LectureCard
-              key={lectureData.id}
-              lectureData={lectureData}
-              type={type}
-            />
-          );
-        })}
+      {lectureListData.map((lectureData) => {
+        return (
+          <LectureCard
+            key={lectureData.id}
+            lectureData={lectureData}
+            type={type}
+          />
+        );
+      })}
     </div>
   ) : (
     <div className="w-full grid grid-cols-2 gap-6 mobile:grid-cols-1 tablet:grid-cols-1 desktop:grid-cols-2 pb-[265px]">
-      {lectureListData &&
-        lectureListData.map((lectureData) => {
-          return (
-            <LectureCard
-              key={lectureData.id}
-              lectureData={lectureData}
-              type={type}
-            />
-          );
-        })}
+      {lectureListData.map((lectureData) => {
+        return (
+          <LectureCard
+            key={lectureData.id}
+            lectureData={lectureData}
+            type={type}
+          />
+        );
+      })}
     </div>
   );
 };
