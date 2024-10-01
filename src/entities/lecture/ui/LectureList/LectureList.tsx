3c56d4e62372e0b0efa2fff8a9a529cdedee3c31
Lawrene Jang <jkb2221@gmail.com>
2024-10-01 18:09:00 +0900

    🐛 fix: refactor lecture list components

--- src/entities/lecture/ui/LectureList/LectureList.tsx
+++ src/entities/lecture/ui/LectureList/LectureList.tsx
@@ -7,20 +7,15 @@ interface LectureListProps {
 }
 
 const LectureList = ({ lectureListData, type }: LectureListProps) => {
-  return type === "col" ? (
-    <div className="w-full grid grid-cols-3 gap-6 mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3">
-      {lectureListData.map((lectureData) => {
-        return (
-          <LectureCard
-            key={lectureData.id}
-            lectureData={lectureData}
-            type={type}
-          />
-        );
-      })}
-    </div>
-  ) : (
-    <div className="w-full grid grid-cols-2 gap-6 mobile:grid-cols-1 tablet:grid-cols-1 desktop:grid-cols-2 pb-[265px]">
+  const setClassNameByType = () => {
+    if (type === "col") {
+      return `w-full grid grid-cols-3 gap-6 mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3`;
+    }
+    return `w-full grid grid-cols-2 gap-6 mobile:grid-cols-1 tablet:grid-cols-1 desktop:grid-cols-2 pb-[265px]`;
+  };
+
+  return (
+    <div className={setClassNameByType()}>
       {lectureListData.map((lectureData) => {
         return (
           <LectureCard
