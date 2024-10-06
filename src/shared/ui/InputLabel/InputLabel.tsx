8fef6b73eadbf7aa7b71ee9c9ef4ebff568f841e
Lawrene Jang <jkb2221@gmail.com>
2024-10-07 03:22:57 +0900

    ✨ feat: update InputLabel components to use react-hook-form

--- src/shared/ui/InputLabel/InputLabel.tsx
+++ src/shared/ui/InputLabel/InputLabel.tsx
@@ -1,4 +1,5 @@
 import { Input } from "..";
+import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface InputLabelProps {
@@ -8,78 +9,84 @@ interface InputLabelProps {
   status?: InputLabelStatus;
   type?: string;
   message?: string;
+  value?: string;
+  error?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // onChange 핸들러 추가
+  onBlur?: () => void; // onBlur 핸들러 추가
 }
 
-type InputLabelStatus = "default" | "correct" | "error";
+export type InputLabelStatus = "default" | "correct" | "error";
 
-const InputLabel = ({
-  labelContent,
-  placeholder,
-  disabled,
-  status = "default",
-  type = "text",
-  message,
-}: InputLabelProps) => {
-  const containerClassName = () => {
-    if (message) {
-      return "flex flex-col w-full gap-2";
-    }
-    return "flex flex-col w-full";
-  };
-
-  const borderColor = () => {
-    if (disabled) {
-      return "border-custom-disabled";
-    }
-    if (status === "error") {
-      return "border-custom-error";
-    }
-    if (status === "correct") {
-      return "border-custom-correct";
-    }
-    return "border-custom-purple";
-  };
-
-  const textColor = () => {
-    if (disabled) {
-      return "text-custom-textDescriptionGrayColor";
-    }
-  };
-
-  const messageTextColor = () => {
+const InputLabel = forwardRef<HTMLInputElement, InputLabelProps>(
+  (
     {
+      labelContent,
+      placeholder,
+      value,
+      disabled,
+      status = "default",
+      type = "text",
+      message,
+      error,
+      onChange,
+      onBlur,
+    },
+    ref,
+  ) => {
+    const containerClassName = () => {
+      return "flex flex-col w-full gap-2";
+    };
+
+    const borderColor = () => {
+      if (disabled) {
+        return "border-custom-disabled";
+      }
+      if (status === "error") {
+        return "border-custom-error";
+      }
+      if (status === "correct") {
+        return "border-custom-correct";
+      }
+      return "border-custom-purple";
+    };
+
+    const textColor = () => {
       if (status === "error") {
         return "text-custom-error";
       }
+
       if (status === "correct") {
         return "text-custom-correct";
       }
-    }
-  };
+    };
 
-  return (
-    <div className={containerClassName()}>
-      <div className="flex flex-col w-full">
-        <div className="text-base h-[21px] text-custom-textSemiBoldBlackColor">
-          {labelContent}
+    return (
+      <div className={containerClassName()}>
+        <div className="flex flex-col w-full">
+          <div className="text-base h-[21px] text-custom-textSemiBoldBlackColor">
+            {labelContent}
+          </div>
+          <div className={twMerge(`flex w-full border-b-2`, borderColor())}>
+            <Input
+              type={type}
+              placeholder={placeholder}
+              value={value}
+              disabled={disabled}
+              className="text-xl h-14 border-none shadow-none focus-visible:ring-0"
+              onChange={onChange}
+              onBlur={onBlur} // onBlur 호출
+              ref={ref}
+            />
+          </div>
         </div>
-        <div className={twMerge(`flex w-full border-b-2`, borderColor())}>
-          <Input
-            type={type}
-            placeholder={placeholder}
-            disabled={disabled}
-            className={twMerge(
-              "text-xl h-14 border-none shadow-none focus-visible:ring-0",
-              textColor(),
-            )}
-          />
+        <div className={twMerge("flex w-full text-base", textColor())}>
+          {message} {/* 오류 메시지 표시 */}
         </div>
       </div>
-      <div className={twMerge("flex w-full text-base", messageTextColor())}>
-        {message}
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
+
+InputLabel.displayName = "InputLabel";
 
 export default InputLabel;
