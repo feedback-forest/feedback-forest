b30b3e2bc78719db0996d87e56c1c755a1e38a08
Lawrene Jang <jkb2221@gmail.com>
2024-10-07 02:35:09 +0900

    ✨ feat: add lecture api hooks

--- src/entities/lecture/api/useLectureList.ts
+++ src/entities/lecture/api/useLectureList.ts
@@ -1,14 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { GetLectureList } from "../model/lecture";
 import { LECTURE_KEYS } from "@/shared/api/keyFactory";
 import { getLectureList } from ".";
-import { useQuery } from "@tanstack/react-query";
 
 const useLectureList = () => {
-  return useQuery({
-    queryKey: LECTURE_KEYS.list(),
-    queryFn: () => getLectureList(),
-    select: (response) => response.data,
-    meta: {
-      errorMessage: "Failed to fetch Lecture List",
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (lectureListReq: GetLectureList["Request"]["body"]) =>
+      getLectureList({
+        payload: lectureListReq.payload,
+        params: lectureListReq.params,
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: LECTURE_KEYS.lists() });
+      // TODO: 무한 스크롤 처리하면서 queryKey 바꿔줘야됨
+      queryClient.invalidateQueries({
+        queryKey: LECTURE_KEYS.list({ page: 1, size: 10 }),
+      });
     },
   });
 };
