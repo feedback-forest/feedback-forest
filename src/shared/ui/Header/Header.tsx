8602ecfef8ae90a7384c25ab8ef29b37dfd14546
Lawrene Jang <jkb2221@gmail.com>
2024-10-07 11:08:19 +0900

    💎 style: update Header Logo

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Logo } from "./Logo";
 
 const Header = () => {
   return (
-    <div className="flex w-full h-16 justify-between items-center gap-2 p-2 fixed top-0 bg-white z-10 px-[120px] mobile:px-4">
+    <div className="flex w-full h-[70px] justify-between items-center gap-2 p-2 fixed top-0 bg-white z-10 px-[120px] mobile:px-4">
       <Logo />
       <HeaderDescription />
       <HeaderFeatures />
--- src/shared/ui/Header/Logo/Logo.tsx
+++ src/shared/ui/Header/Logo/Logo.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="flex flex-col w-20 min-w-[55px] justify-center items-center gap-2">
+    <div className="flex flex-col justify-center items-center desktop:w-[94px] desktop:h-[70px] desktop:min-w-[94px] desktop:min-h-[70px] tablet:w-[94px] tablet:h-[70px] tablet:min-w-[94px] tablet:min-h-[70px] mobile:w-[57px] mobile:h-[48px] mobile:min-w-[57px] mobile:min-h-[48px]">
       <Link href="/">
         <Image
-          src="/images/sijak_logo.png"
+          src="/icons/sijak_logo.svg"
           alt="sijak_logo"
-          width={94}
-          height={80}
+          width={75}
+          height={51}
         />
       </Link>
     </div>
