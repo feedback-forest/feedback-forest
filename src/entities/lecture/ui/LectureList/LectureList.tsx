9d4be3fa35314bc435640a44fa0fedfbe3e10bdd
Lawrene Jang <jkb2221@gmail.com>
2024-10-08 18:49:26 +0900

    ♻️ refactor: split lecture card to HomeLectureCard and PickLectureCard

--- src/entities/lecture/ui/LectureList/LectureList.tsx
+++ src/entities/lecture/ui/LectureList/LectureList.tsx
@@ -8,19 +8,12 @@ interface LectureListProps {
     | LectureInfo[]
     | PickLectureInfo[]
     | HeartsLectureListResDataInfo[];
-  type: "row" | "col";
+  type: "homeLecture" | "pickLecture";
 }
 
 const LectureList = ({ lectureListData, type }: LectureListProps) => {
-  const setClassNameByType = () => {
-    if (type === "col") {
-      return `w-full grid grid-cols-3 gap-6 mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3`;
-    }
-    return `w-full grid grid-cols-2 gap-6 mobile:grid-cols-1 tablet:grid-cols-1 desktop:grid-cols-2 pb-[265px]`;
-  };
-
   return (
-    <div className={setClassNameByType()}>
+    <div className="w-full grid grid-cols-3 gap-6 mobile:grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3">
       {lectureListData.map((lectureData) => {
         return (
           <LectureCard
